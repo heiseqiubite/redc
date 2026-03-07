bb6cb629dcc843799123bcf4f75e1bd90ace3f98
r0fus0d <d2hwakh7bs5e@protonmail.com>
2026-03-07 22:17:54 +0800

    feat(日志): 改进日志功能以支持自定义路径

--- mod/redclog.go
+++ mod/redclog.go
@@ -3,14 +3,23 @@ package mod
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 	"red-cloud/mod2"
 	"time"
 )
 
-// RedcLog 将给定的消息记录到 "redc.log" 文件中。
+// RedcLog 将给定的消息记录到日志文件中
 func RedcLog(message string) {
+	logPath := filepath.Join(RedcPath, "redc.log")
+	
+	// 确保目录存在
+	if err := os.MkdirAll(filepath.Dir(logPath), 0755); err != nil {
+		mod2.PrintOnError(err, "failed to create log directory")
+		return
+	}
+	
 	// 打开或创建日志文件
-	file, err := os.OpenFile("redc.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	file, err := os.OpenFile(logPath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	mod2.PrintOnError(err, "failed to open log file")
 	defer file.Close()
 
